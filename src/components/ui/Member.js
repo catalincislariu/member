e10eaa88993dd4980cc0a3d217fb0b59d6972447
Catalin Cislariu <catalincislariu@gmail.com>
2018-06-25 11:26:30 +0100

    Member & Member List

--- src/components/ui/Member.js
+++ src/components/ui/Member.js
@@ -1,19 +1,25 @@
 import FaShield from 'react-icons/lib/fa/shield'
 import { Component } from 'react'
 
-export class Member extends Component {
+class Member extends Component {
 
-render() {
-		const { name, thumbnail, email, admin, makeAdmin } = this.props
-    return (
-        <div className="member">
-					<h1>{name} {(admin) ? <FaShield /> : null}</h1>
-					<a onClick={makeAdmin}>Make Admin</a>
-					<img src={thumbnail} alt="Profile Picture"/>
-					<p><a href={`mailto:${email}`}>{email}</a></p>
-        </div>
-    )
-}
+	componentWillMount() {
+		this.style = {
+			backgroundColor: 'gray'
+		}
+	}
+
+	render() {
+			const { name, thumbnail, email, admin, makeAdmin } = this.props
+	    return (
+	        <div className="member" style={this.style}>
+						<h1>{name} {(admin) ? <FaShield /> : null}</h1>
+						<a onClick={makeAdmin}>Make Admin</a>
+						<img src={thumbnail} alt="Profile Picture"/>
+						<p><a href={`mailto:${email}`}>{email}</a></p>
+	        </div>
+	    )
+	}
 }
 
 export default Member
